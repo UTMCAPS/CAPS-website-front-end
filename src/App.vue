89b5f879e6460f44a142612a1171f25646f3ac7f
Zhengyu (Joey) Wang <95310061+wzy403@users.noreply.github.com>
2024-12-31 01:27:14 -0500

    Fix style issue (#18)
    * Move topNavBar and contact bar to App.vue
    
    * Change logo src
    
    * fix icon center misaligned

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <template>
   <div id="app">
+    <div class="contact">
+      <HomePageContact/>
+    </div>
+    <div class="nav">
+      <NavigationBar @logoClicked="logoClicked" />
+    </div>
     <router-view class="router"/>
   </div>
 </template>
 
 <script>
+import HomePageContact from "@/components/homePage/HomePageContact.vue";
+import NavigationBar from "@/components/concrete/NavigationBar.vue";
+
 export default {
   name: "App",
   components: {
+    HomePageContact,
+    NavigationBar,
+  },
+  data() {
+    return {
+      scroll: true
+    }
+  },
+  methods: {
+    scrollToNav() {
+      const nav = document.querySelector('.nav');
+      if(this.scroll && nav){
+        nav.scrollIntoView({ behavior: 'smooth' });
+        this.scroll = false;
+      }
+    },
+    logoClicked() {
+      this.scroll = true;
+      this.scrollToNav();
+    }
+  },
+  watch: {
+    $route() {
+      this.$nextTick(() => {
+        this.scrollToNav();
+      });
+    }
   },
 };
 </script>
@@ -17,8 +53,21 @@ body {
   margin: 0;
   padding: 0;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   overflow-x: hidden;
+}
+.contact {
+  width: 100vw;
+  height: 5vh;
+  background: #1F3979;
+}
+.nav {
+  width: 100vw;
+  height: 10vh;
+  background: #EEEEEE;
+  position: sticky;
+  top: 0;
+  z-index: 1;
 }
 #app {
   font-family: Arial, sans-serif;
