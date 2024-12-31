0f7785aa2dc7a42ae958e339b835791c3cb0396e
wurenfei <chrisren.wu@mail.utoronto.ca>
2024-12-30 20:49:21 -0500

    add arrow to upcoming event

--- src/components/concrete/ShowCaseBlock.vue
+++ src/components/concrete/ShowCaseBlock.vue
@@ -38,7 +38,7 @@ export default {
       <div class="description">
         <p>{{ description }}</p>
       </div>
-      <router-link class="button" to={{page}} >
+      <router-link class="button" to={{pge}} >
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#024BAF"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
       </router-link>
     </div>
--- src/components/concrete/ShowCaseQueue.vue
+++ src/components/concrete/ShowCaseQueue.vue
@@ -3,6 +3,25 @@ import { ref } from "vue";
 import ShowCaseBlock from "@/components/concrete/ShowCaseBlock.vue";
 
 const boxRef = ref(null);
+const scrollToNext = () => {
+  if (boxRef.value) {
+    const scrollAmount = boxRef.value.querySelector('.queue-element').offsetWidth; 
+    boxRef.value.scrollBy({
+      left: scrollAmount,
+      behavior: "smooth", 
+    });
+  }
+};
+const scrollToPrevious = () => {
+  if (boxRef.value) {
+    const scrollAmount = boxRef.value.querySelector('.queue-element').offsetWidth; 
+    boxRef.value.scrollBy({
+      left: -scrollAmount,
+      behavior: "smooth", 
+    });
+  }
+};
+
 </script>
 
 <template>
@@ -29,6 +48,12 @@ const boxRef = ref(null);
       </div>
     </div>
   </div>
+  <button class="prev" @click="scrollToPrevious">
+    <img src="@/assets/svg/keyboard_arrow_left_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt="Previous" class="arrow-icon">
+  </button>
+  <button class="next" @click="scrollToNext">
+    <img src="@/assets/svg/keyboard_arrow_right_24dp_2C3E50_FILL0_wght400_GRAD0_opsz24.svg" alt="Next" class="arrow-icon">
+  </button>
 </template>
 
 <style scoped>
@@ -55,4 +80,38 @@ const boxRef = ref(null);
   justify-content: center;
   align-items: center;
 }
+.prev, .next {
+  width: 8%;
+  height: 8%;
+  background: none;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  opacity: 0.2;
+  transition: 0.3s;
+}
+
+.prev {
+  left: 3%;
+}
+.next {
+  right: 3%;
+}
+
+.arrow-icon {
+  width: 300%;
+  height: 300%;
+  fill: #1F3979;
+}
+
+.prev:hover, .next:hover {
+  opacity: 0.6;
+  transform: scale(1.1);
+}
+
+
 </style>
